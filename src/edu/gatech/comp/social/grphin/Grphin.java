568c70dd8b5d8dd6426f95b15bd4607fd5440c89
Andrew <aliuy8@gmail.com>
2012-12-05 05:21:59 -0500

    First Commit - Json Parser

---
+++ src/edu/gatech/comp/social/grphin/Grphin.java
@@ -0,0 +1,266 @@
+package edu.gatech.comp.social.grphin;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class Grphin {
+  // Change this to your dataset (can be a directory or individual file).
+  private final static String DATA = "C:\\Users\\andrew\\Desktop\\1339943811\\";
+  // Change this to your output file.
+  private final static String OUTPUT = "C:\\Users\\andrew\\Desktop\\jsonOutput.json";
+  // The minimum number of employees needed in order to output a company or
+  // edge.
+  private final static int THRESHOLD = 2;
+
+  // Candidate company names to be normalized.
+  public static Set<String> NORMAL_CANDIDATES = new HashSet<String>();
+  // Change this to your output file.
+  private final static String NORMAL_OUTPUT = "C:\\Users\\andrew\\Desktop\\normal.txt";
+
+  /**
+   * The Main Method
+   */
+  public static void main(String[] args) {
+    Grphin driver = new Grphin();
+    long timer = System.currentTimeMillis();
+
+    // Parse Input
+    System.out.println("Processing " + DATA);
+    driver.parse(new File(DATA));
+
+    // TODO: Add in database support for smarter filtering.
+
+    // Output File
+    System.out.println("Outputting to " + OUTPUT);
+    try {
+      FileWriter out = new FileWriter(OUTPUT);
+      out.write(driver.toString());
+      out.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    // Output Normal File
+    try {
+      FileWriter out = new FileWriter(NORMAL_OUTPUT);
+      for (String normal : NORMAL_CANDIDATES) {
+        out.write(normal + "\n");
+      }
+      out.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    timer = (System.currentTimeMillis() - timer) / 1000;
+    System.out.println("Finished processing ~" + driver.getPeople().size() + " profiles!");
+    System.out.println("Elapsed Time: " + timer + " seconds.");
+
+  }
+
+  // Set<PersonId> people.
+  private Set<String> people = new HashSet<String>();
+
+  // Map<CompanyName, HashSet<PersonId>> companies.
+  private Map<String, HashSet<String>> nodes = new HashMap<String, HashSet<String>>();
+
+  // Map<Edge, HashSet<PersonId>> edges.
+  private Map<Edge, HashSet<String>> edges = new HashMap<Edge, HashSet<String>>();
+
+  // State Date format strings.
+  private String[] formatStrings = { "yyyy-MM-dd", "yyyy" };
+
+  public Set<String> getPeople() {
+    return people;
+  }
+
+  /**
+   * Parses a directory or file.
+   * 
+   * @param filePath the File Path to a file or directory.
+   */
+  private void parse(File filePath) {
+    if (filePath.isDirectory()) {
+      for (File file : filePath.listFiles()) {
+        parse(file);
+      }
+    } else {
+      parseFile(filePath);
+    }
+  }
+
+  /**
+   * Parses a single file.
+   * 
+   * @param myFile the File Path to the file.
+   */
+  private void parseFile(File myFile) {
+    try {
+      JsonParser parser = new JsonParser();
+      JsonElement jsonElement = parser.parse(new FileReader(myFile));
+      if (jsonElement.isJsonArray()) {
+        for (JsonElement e : jsonElement.getAsJsonArray()) {
+          if (e.isJsonObject()) {
+            JsonObject jsonObj = e.getAsJsonObject();
+            // Employee Id: Use profile-url as unique identifier.
+            if (jsonObj.get("public-profile-url") != null
+                && jsonObj.get("public-profile-url").isJsonPrimitive()) {
+              String employeeId = jsonObj.get("public-profile-url").getAsString();
+
+              // Add people to a HashSet for statistical purposes.
+              people.add(employeeId);
+
+              // If Employee ID exists, check for positions.
+              PriorityQueue<Company> sortedCompanies = new PriorityQueue<Company>();
+              if (jsonObj.get("positions") != null && jsonObj.get("positions").isJsonArray()) {
+                for (JsonElement position : jsonObj.get("positions").getAsJsonArray()) {
+                  if (position.isJsonObject()) {
+                    JsonObject company = position.getAsJsonObject();
+                    if (company.get("company-name") != null
+                        && company.get("company-name").isJsonPrimitive()
+                        && company.get("start-date") != null
+                        && company.get("start-date").isJsonPrimitive()) {
+                      // Strip formatting to help normalize company names.
+                      String companyName = NameUtil.normalize(company.get("company-name")
+                          .getAsString());
+                      Date startDate = tryParse(company.get("start-date").getAsString());
+                      sortedCompanies.add(new Company(companyName, startDate));
+                    }
+
+                  }
+                }
+              }
+
+              if (sortedCompanies.size() > 0) {
+                Company c = sortedCompanies.poll();
+                // Add to employee to company map
+                HashSet<String> companyEmployees = nodes.get(c.name);
+                if (companyEmployees == null) {
+                  companyEmployees = new HashSet<String>();
+                }
+                companyEmployees.add(employeeId);
+                nodes.put(c.name, companyEmployees);
+
+                // Set oldCompany or newCompany?
+                Company oldC;
+                while (sortedCompanies.size() > 0) {
+                  oldC = c;
+                  c = sortedCompanies.poll();
+
+                  // Add to employee to company map
+                  companyEmployees = nodes.get(c.name);
+                  if (companyEmployees == null) {
+                    companyEmployees = new HashSet<String>();
+                  }
+                  companyEmployees.add(employeeId);
+                  nodes.put(c.name, companyEmployees);
+
+                  // Skip if source == destination (most likely a promotion).
+                  if (!(oldC.equals(c)) && oldC.name.length() > 0 && c.name.length() > 0) {
+                    // Add to employee to edge map
+                    Edge key = new Edge();
+                    key.source = oldC.name;
+                    key.destination = c.name;
+                    HashSet<String> edgeEmployees = edges.get(key);
+                    if (edgeEmployees == null) {
+                      edgeEmployees = new HashSet<String>();
+                    }
+                    edgeEmployees.add(employeeId);
+                    edges.put(key, edgeEmployees);
+                  }
+                }
+              }
+            }
+          }
+        }
+        // System.out.println("Processed: " + myFile.getName());
+      } else {
+        // Invalid JSON
+        System.out.println("Error: Invalid Json - " + myFile.getName());
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  public String toString() {
+    JsonArray toRet = new JsonArray();
+    Map<String, Company> companyMap = new HashMap<String, Company>();
+
+    // Build Company Objects
+    for (String companyName : nodes.keySet()) {
+      Integer companySize = nodes.get(companyName).size();
+      // Skip companies that have too few employees (uninteresting).
+      if (companySize > THRESHOLD) {
+        companyMap.put(companyName, new Company(companyName, companySize));
+      }
+    }
+
+    // Build Incoming Edges
+    for (Edge e : edges.keySet()) {
+      Company c = companyMap.get(e.destination);
+      Integer edgeSize = edges.get(e).size();
+      // Skip edges that have too few employees (uninteresting).
+      if (c != null && edgeSize > THRESHOLD) {
+
+        // Add normalization candidates as needed.
+        if (e.destination.contains(e.source) || e.source.contains(e.destination)) {
+          if (e.source.length() < e.destination.length()) {
+            NORMAL_CANDIDATES.add("normalizedNames.put(\"" + e.destination + "\", \"" + e.source
+                + "\");");
+          } else {
+            NORMAL_CANDIDATES.add("normalizedNames.put(\"" + e.source + "\", \"" + e.destination
+                + "\");");
+          }
+        }
+
+        c.incomingEdges.put(e.source, edgeSize);
+      }
+    }
+
+    // Build Json
+    for (Company c : companyMap.values()) {
+      if (c.incomingEdges.size() > 0) {
+        JsonObject companyJson = new JsonObject();
+        companyJson.addProperty("company-name", c.name);
+        JsonArray incoming = new JsonArray();
+        for (String in : c.incomingEdges.keySet()) {
+          JsonObject inCompany = new JsonObject();
+          inCompany.addProperty("in-company", in);
+          inCompany.addProperty("count", c.incomingEdges.get(in));
+          incoming.add(inCompany);
+        }
+        companyJson.add("incoming", incoming);
+        toRet.add(companyJson);
+      }
+    }
+
+    return toRet.toString();
+  }
+
+  private Date tryParse(String dateString) {
+    for (String formatString : formatStrings) {
+      try {
+        return new SimpleDateFormat(formatString).parse(dateString);
+      } catch (ParseException e) {
+      }
+    }
+
+    return null;
+  }
+}
---
+++ src/edu/gatech/comp/social/grphin/NameUtil.java
@@ -0,0 +1,129 @@
+package edu.gatech.comp.social.grphin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NameUtil {
+  // Map of normalized names (Microsoft Corporation -> Microsoft)
+  private static Map<String, String> normalizedNames;
+
+  public static String normalize(String name) {
+    if (normalizedNames == null) {
+      normalizedNames = new HashMap<String, String>();
+      normalizedNames.put("ABNAMROBank", "ABNAMRO");
+      normalizedNames.put("ABNAMROBankNV", "ABNAMRO");
+      normalizedNames.put("AccentureInc", "Accenture");
+      normalizedNames.put("AccentureTechnologySolutions", "Accenture");
+      normalizedNames.put("AdeccoGroup", "Adecco");
+      normalizedNames.put("AdobeSystems", "Adobe");
+      normalizedNames.put("AgilityLogistics", "Agility");
+      normalizedNames.put("AlcatelLucent", "Alcatel");
+      normalizedNames.put("AmericanExpressBusinessTravel", "AmericanExpress");
+      normalizedNames.put("AppleInc", "Apple");
+      normalizedNames.put("AppleRetail", "Apple");
+      normalizedNames.put("ArabBankPLC", "ArabBank");
+      normalizedNames.put("ArabBankplc", "ArabBank");
+      normalizedNames.put("AricentGroup", "Aricent");
+      normalizedNames.put("AstraZenecaPharmaceuticals", "AstraZeneca");
+      normalizedNames.put("AstraZenecaRD", "AstraZeneca");
+      normalizedNames.put("ATTMobility", "ATT");
+      normalizedNames.put("BancoBradesco", "Bradesco");
+      normalizedNames.put("BarclaysBank", "Barclays");
+      normalizedNames.put("BritishAmericanTobaccoNigeriaLimited", "BritishAmericanTobacco");
+      normalizedNames.put("CapgeminiConsulting", "Capgemini");
+      normalizedNames.put("CapgeminiIndiaPvtLtd", "Capgemini");
+      normalizedNames.put("CiscoSystems", "Cisco");
+      normalizedNames.put("CiscoSystemsInc", "Cisco");
+      normalizedNames.put("Citibank", "Citi");
+      normalizedNames.put("CitibankNA", "Citi");
+      normalizedNames.put("Citigroup", "Citi");
+      normalizedNames.put("CognizantTechnologySolutions", "Cognizant");
+      normalizedNames.put("DanskeBankGroup", "DanskeBank");
+      normalizedNames.put("DellInc", "Dell");
+      normalizedNames.put("DeloitteConsulting", "Deloitte");
+      normalizedNames.put("DeloitteLLP", "Deloitte");
+      normalizedNames.put("DeloitteTouche", "Deloitte");
+      normalizedNames.put("DeloitteToucheLLP", "Deloitte");
+      normalizedNames.put("DHLExpress", "DHL");
+      normalizedNames.put("EricssonEgypt", "Ericsson");
+      normalizedNames.put("EricssonMalaysia", "Ericsson");
+      normalizedNames.put("EricssonNigeria", "Ericsson");
+      normalizedNames.put("EtisalatNigeria", "Etisalat");
+      normalizedNames.put("GlobacomLimited", "Globacom");
+      normalizedNames.put("HCLTechnologiesLtd", "HCLTechnologies");
+      normalizedNames.put("HoffmannLaRoche", "Roche");
+      normalizedNames.put("HSBCBank", "HSBC");
+      normalizedNames.put("IBMArgentina", "IBM");
+      normalizedNames.put("IBMAustralia", "IBM");
+      normalizedNames.put("IBMBrasil", "IBM");
+      normalizedNames.put("IBMBusinessConsultingServices", "IBM");
+      normalizedNames.put("IBMCanadaLtd", "IBM");
+      normalizedNames.put("IBMGlobalBusinessServices", "IBM");
+      normalizedNames.put("IBMGlobalProcessServices", "IBM");
+      normalizedNames.put("IBMGlobalServices", "IBM");
+      normalizedNames.put("IBMIndia", "IBM");
+      normalizedNames.put("IBMIndiaPvtLtd", "IBM");
+      normalizedNames.put("IBMSoftwareGroup", "IBM");
+      normalizedNames.put("IBMTivoliSoftware", "IBM");
+      normalizedNames.put("ICICIBankLtd", "ICICIBank");
+      normalizedNames.put("IndianaUniversityBloomington", "IndianaUniversity");
+      normalizedNames.put("InfosysLimited", "Infosys");
+      normalizedNames.put("InfosysTechnologiesLtd", "Infosys");
+      normalizedNames.put("IntelCorporation", "Intel");
+      normalizedNames.put("JPMorganChase", "JPMorgan");
+      normalizedNames.put("KPITCumminsInfosystemsLimited", "KPITCummins");
+      normalizedNames.put("KPMGAdvisory", "KPMG");
+      normalizedNames.put("KPMGAudit", "KPMG");
+      normalizedNames.put("KPMGLLP", "KPMG");
+      normalizedNames.put("KPMGProfessionalServices", "KPMG");
+      normalizedNames.put("LMEricssonNigeria", "Ericsson");
+      normalizedNames.put("LogicaCMG", "Logica");
+      normalizedNames.put("MicrosoftCorporation", "Microsoft");
+      normalizedNames.put("MotorolaMobility", "Motorola");
+      normalizedNames.put("MotorolaSolutions", "Motorola");
+      normalizedNames.put("MTNNigeria", "MTN");
+      normalizedNames.put("NortelNetworks", "Nortel");
+      normalizedNames.put("NovoNordiskAS", "NovoNordisk");
+      normalizedNames.put("NykreditRealkreditAS", "Nykredit");
+      normalizedNames.put("OracleCorporation", "Oracle");
+      normalizedNames.put("PatniComputerSystemsLtd", "PatniComputerSystems");
+      normalizedNames.put("PfizerInc", "Pfizer");
+      normalizedNames.put("PfizerPharmaceuticals", "Pfizer");
+      normalizedNames.put("PTBankCIMBNiagaTbk", "PTBankCIMBNiaga");
+      normalizedNames.put("PTTelkomsel", "Telkomsel");
+      normalizedNames.put("RochePharmaceuticals", "Roche");
+      normalizedNames.put("SafecoInsurance", "Safeco");
+      normalizedNames.put("SAPLabs", "SAP");
+      normalizedNames.put("SchlumbergerInformationSolutions", "Schlumberger");
+      normalizedNames.put("SprintNextel", "Sprint");
+      normalizedNames.put("StarbucksCoffeeCompany", "Starbucks");
+      normalizedNames.put("TechMahindraLtd", "TechMahindra");
+      normalizedNames.put("TescoStoresLtd", "Tesco");
+      normalizedNames.put("TheAmericanUniversityinCairo", "AmericanUniversityinCairo");
+      normalizedNames.put("TheHomeDepot", "HomeDepot");
+      normalizedNames.put("TheUniversityofJordan", "UniversityofJordan");
+      normalizedNames.put("TravelersInsurance", "Travelers");
+      normalizedNames.put("TRWAutomotive", "TRW");
+      normalizedNames.put("UBSInvestmentBank", "UBS");
+      normalizedNames.put("UnileverMashreq", "Unilever");
+      normalizedNames.put("UniversityofGhanaLegon", "UniversityofGhana");
+      normalizedNames.put("USNavyReserve", "USNavy");
+      normalizedNames.put("VerizonBusiness", "Verizon");
+      normalizedNames.put("VodafoneEgypt", "Vodafone");
+      normalizedNames.put("VodafoneIT", "Vodafone");
+      normalizedNames.put("WachoviaAWellsFargoCompany", "WellsFargo");
+      normalizedNames.put("WellsFargoBank", "WellsFargo");
+      normalizedNames.put("WellsFargoHomeMortgage", "WellsFargo");
+      normalizedNames.put("WesternUnionFinancialServices", "WesternUnion");
+      normalizedNames.put("WyethPharmaceuticals", "Wyeth");
+      normalizedNames.put("ZainJordan", "Zain");
+      normalizedNames.put("Zainjordan", "Zain");
+      normalizedNames.put("ZainNigeria", "Zain");
+    }
+    name = name.replaceAll("[^a-zA-Z0-9]", "");
+    if (normalizedNames.containsKey(name)) {
+      name = normalizedNames.get(name);
+    }
+    return name;
+  }
+}
